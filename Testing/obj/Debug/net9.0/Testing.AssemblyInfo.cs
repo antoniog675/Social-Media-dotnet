cb1d5053af2d9d49267d533568d03a75a7d2b9fe
Antonio Gomez <antoniogomez1@hotmail.co.uk>
2026-06-02 13:37:02 +0100

    pushing changes

--- Testing/obj/Debug/net9.0/Testing.AssemblyInfo.cs
+++ Testing/obj/Debug/net9.0/Testing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f25ecf6a27025e0563b686f34633d001cf06f244")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
